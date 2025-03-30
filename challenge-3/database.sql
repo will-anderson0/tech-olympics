225a75743e4fc08a46ae3919c4dee5beab97f317
William Anderson <wanderson.wa252@gmail.com>
2025-03-30 12:35:24 -0400

    q1 done

--- challenge-3/database.sql
+++ challenge-3/database.sql
@@ -1,6 +1,16 @@
 -- given the following table definitions, write a query to find the percentage of users who returned to log in within 7 days of their first login
-create table logins (
-    user_id INT NOT NULL,
-    login_date DATE NOT NULL,
-    PRIMARY KEY (user_id, login_date)
-);
+WITH user_activity AS (
+    SELECT 
+        user_id,
+        MIN(login_date) as first_login,
+        MAX(CASE WHEN login_date <= MIN(login_date) OVER (PARTITION BY user_id) + INTERVAL '7 days' 
+            THEN login_date END) as last_login_within_week
+    FROM logins
+    GROUP BY user_id
+)
+SELECT 
+    ROUND(
+        (COUNT(CASE WHEN last_login_within_week > first_login THEN 1 END)::FLOAT / COUNT(*)::FLOAT) * 100,
+        2
+    ) as return_percentage
+FROM user_activity;
